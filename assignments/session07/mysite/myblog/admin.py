49e5d914d544f05a5aa2b19901779913ef7e40e4
dignoto <dignoto@gmail.com>
2014-02-24 21:50:39 -0800

    hw 7, required tasks completed

--- assignments/session07/mysite/myblog/admin.py
+++ assignments/session07/mysite/myblog/admin.py
@@ -2,5 +2,19 @@ from django.contrib import admin
 from myblog.models import Post
 from myblog.models import Category
 
-admin.site.register(Post)
-admin.site.register(Category)
+
+class CategoryAdmin(admin.ModelAdmin):
+    # Suppress the 'post' field.
+    exclude = ('posts',)
+
+class CategoryInline(admin.TabularInline):
+    # Display the many-to-many relations, and use the 'through' attribute to
+    # reference the built in model that manages the many-to-many relation.
+    model = Category.posts.through
+
+class PostAdmin(admin.ModelAdmin):
+   # Use 'inlines' so that we can edit the Category model from the Post page.
+   inlines = [CategoryInline,]
+       
+admin.site.register(Post, PostAdmin)
+admin.site.register(Category, CategoryAdmin)
